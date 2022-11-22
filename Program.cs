ab9e2b46a8ad20479f7d513d167883f5f9cc18a7
Hp <Hp@DESKTOP-ID5FAVQ>
2022-11-22 10:07:22 +0100

    tache

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Examen
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Accueil());
+            Application.Run(new Form1());
 
 
         }
